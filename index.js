a1c2357908a0ea009ea3bae9534687c675fccd9a
Vivek <vs690799@gmail.com>
2025-05-29 04:38:42 +0530

    add dotenv, express, and mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { myLogger } from "./middleware/logger.js";
+import employeeRouter from "./routes/employee.js";
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,8 @@ app.use(myLogger);
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+app.use("/", employeeRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
