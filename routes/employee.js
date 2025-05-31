75bf3cc7001e4d9b15a9975c76aa251dff7648b9
Vivek <vs690799@gmail.com>
2025-05-31 08:52:56 +0530

    add new endpoints

--- routes/employee.js
+++ routes/employee.js
@@ -19,7 +19,7 @@ router.get("/employee", async (req, res) => {
 
 
 
-router.post("employee", async (req, res) => {
+router.post("/employee", async (req, res) => {
   try {
     const { firstname, lastname, email, birthdate, department } = req.body
 
@@ -45,6 +45,61 @@ router.post("employee", async (req, res) => {
     })
   }
 
+})
+
+router.put("/employee", async (req, res) => {
+  try {
+    const { id, firstname, lastname, email, birthdate, department } = req.body
+
+    if (!(id && firstname && lastname && email && birthdate && department)) {
+      return res.send("Please enter the details.")
+    }
+    const updatedEmployee = await Employee.findByIdAndUpdate(id, {
+      firstname,
+      lastname,
+      email,
+      birthdate,
+      department
+    }, {
+      new: true
+    })
+    if (!updatedEmployee) {
+      return res.status(404).json({
+        message: 'User not found'
+      })
+    }
+    res.status(200).json(updatedEmployee)
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      message: "Internal server error."
+    })
+  }
+})
+
+router.delete("/employee", async (req, res) => {
+  try {
+    const { id } = req.body
+
+    if (!id) {
+      return res.status(400).json({
+        message: "Bad request."
+      })
+    }
+    const deleted = await Employee.findByIdAndDelete(id)
+    if (!deleted) {
+      return res.status(404).json({
+        message: 'User not found'
+      })
+    }
+    res.send('User deleted successfully.')
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      message: 'Internal server error.'
+    })
+  }
 })
 
 export default router;
