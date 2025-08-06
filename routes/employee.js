d7a4b1824827afac62e349e6ddf1ae4bce21b69a
Vivek <vs690799@gmail.com>
2025-08-07 04:25:00 +0530

    add new route

--- routes/employee.js
+++ routes/employee.js
@@ -5,101 +5,120 @@ const router = express.Router();
 
 router.get("/employee", async (req, res) => {
   try {
-    const employees = await Employee.find()
+    const employees = await Employee.find();
     return res.status(200).json({
-      employees
-    })
+      employees,
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).json({
-      message: 'Internal Server Error.'
-    })
+      message: "Internal Server Error.",
+    });
   }
 });
 
-
+router.get("/employee/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const employee = await Employee.findById(id);
+    res.status(200).json(employee);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal Server Error.",
+    });
+  }
+});
 
 router.post("/employee", async (req, res) => {
   try {
-    const { firstname, lastname, email, birthdate, department } = req.body
+    const { firstname, lastname, email, birthdate, department } = req.body;
 
     if (!(firstname && lastname && email && birthdate && department)) {
-      return res.send("Please enter the details.")
+      return res.send("Please enter the details.");
     }
-    const checkEmployee = await Employee.findOne({ firstname, email })
+    const checkEmployee = await Employee.findOne({ firstname, email });
     if (checkEmployee) {
       return res.status(409).json({
-        message: "Employee already exists."
-      })
+        message: "Employee already exists.",
+      });
     }
-    const employee = new Employee({ firstname, lastname, email, birthdate, department })
-    await employee.save()
-    return res.status(201).json({
-      message: 'Employee created successfully.',
-      employee
-    })
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({
-      message: 'Internal Server Error'
-    })
-  }
-
-})
-
-router.put("/employee", async (req, res) => {
-  try {
-    const { id, firstname, lastname, email, birthdate, department } = req.body
-
-    if (!(id && firstname && lastname && email && birthdate && department)) {
-      return res.send("Please enter the details.")
-    }
-    const updatedEmployee = await Employee.findByIdAndUpdate(id, {
+    const employee = new Employee({
       firstname,
       lastname,
       email,
       birthdate,
-      department
-    }, {
-      new: true
-    })
+      department,
+    });
+    await employee.save();
+    return res.status(201).json({
+      message: "Employee created successfully.",
+      employee,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+router.put("/employee", async (req, res) => {
+  try {
+    const { id, firstname, lastname, email, birthdate, department } = req.body;
+
+    if (!(id && firstname && lastname && email && birthdate && department)) {
+      return res.send("Please enter the details.");
+    }
+    const updatedEmployee = await Employee.findByIdAndUpdate(
+      id,
+      {
+        firstname,
+        lastname,
+        email,
+        birthdate,
+        department,
+      },
+      {
+        new: true,
+      },
+    );
     if (!updatedEmployee) {
       return res.status(404).json({
-        message: 'User not found'
-      })
+        message: "User not found",
+      });
     }
-    res.status(200).json(updatedEmployee)
-
+    res.status(200).json(updatedEmployee);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).json({
-      message: "Internal server error."
-    })
+      message: "Internal server error.",
+    });
   }
-})
+});
 
 router.delete("/employee", async (req, res) => {
   try {
-    const { id } = req.body
+    const { id } = req.body;
 
     if (!id) {
       return res.status(400).json({
-        message: "Bad request."
-      })
+        message: "Bad request.",
+      });
     }
-    const deleted = await Employee.findByIdAndDelete(id)
+    const deleted = await Employee.findByIdAndDelete(id);
     if (!deleted) {
       return res.status(404).json({
-        message: 'User not found'
-      })
+        message: "User not found",
+      });
     }
-    res.send('User deleted successfully.')
+    res.send("User deleted successfully.");
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).json({
-      message: 'Internal server error.'
-    })
+      message: "Internal server error.",
+    });
   }
-})
+});
 
 export default router;
