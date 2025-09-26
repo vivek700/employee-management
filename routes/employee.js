ff9d71942a205ea0b5775855dc1b4f670fbf8360
Vivek <vivek.etc@proton.me>
2025-09-27 03:09:55 +0530

    WIP: fix bugs

--- routes/employee.js
+++ routes/employee.js
@@ -4,7 +4,7 @@ import Employee from "../models/employee.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log(req.cookies);
+  console.log("this is cookies", req.cookies);
   const userId = req.cookies["better-auth-user"];
   try {
     const employees = await Employee.find({ userId });
